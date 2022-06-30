4b655d4da9b3a7c7c937657a0e14230c5389b0ed
unknown <derek.slauson5@gmail.com>
2022-06-29 20:46:38 -0500

    Basic server set up, connects to MongoDB

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+const port = process.env.PORT || 4000
+
+// DB connection
+const uri = process.env.MONGO_URI
+mongoose.connect(uri)
+const connection = mongoose.connection
+connection.once("open", () => {
+    console.log("Connected to MongoDB")
+})
+
+app.get("/", (_req, res) => {
+    res.status(200).send("Welcome to my reddit clone API")
+})
+
+app.get("*", (_req, res) => {
+    res.status(404).json({ message: "Route not found" })
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
