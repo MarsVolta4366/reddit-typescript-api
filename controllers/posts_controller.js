4a94b00e62375ad0ca1bb49b10ec1f515c03b060
unknown <derek.slauson5@gmail.com>
2022-07-07 19:12:37 -0500

    Created get posts endpoint

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,6 +1,15 @@
 const Post = require("../models/post.model")
-const User = require("../models/user.model")
 const posts = require("express").Router()
+
+// Get posts for home page
+posts.get("/", async (req, res) => {
+    try {
+        const foundPosts = await Post.find({}, "-_id").populate("user", "username -_id")
+        res.status(200).json(foundPosts)
+    } catch {
+        res.status(500).json({ message: "There was a problem getting posts" })
+    }
+})
 
 // Create post
 posts.post("/", async (req, res) => {
