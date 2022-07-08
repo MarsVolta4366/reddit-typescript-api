ade09037ed1f7c8a43171fef6c929696def46ed9
unknown <derek.slauson5@gmail.com>
2022-07-08 09:23:23 -0500

    Created get post by id endpoint

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,12 +2,22 @@ const Post = require("../models/post.model")
 const posts = require("express").Router()
 
 // Get posts for home page
-posts.get("/", async (req, res) => {
+posts.get("/", async (_req, res) => {
     try {
-        const foundPosts = await Post.find({}, "-_id").populate("user", "username -_id")
+        const foundPosts = await Post.find({}).populate("user", "username -_id")
         res.status(200).json(foundPosts)
     } catch {
         res.status(500).json({ message: "There was a problem getting posts" })
+    }
+})
+
+// Get post by id
+posts.get("/:postId", async (req, res) => {
+    try {
+        const foundPost = await Post.findById(req.params.postId).populate("user", "username -_id")
+        res.status(200).json(foundPost)
+    } catch {
+        res.status(500).json({ message: "Couldn't find post with given id" })
     }
 })
 
